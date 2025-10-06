b3326b9e90ba36241596bd35c83874051446ce22
FUGAMARU <gofugashi@gmail.com>
2025-10-07 01:07:17 +0900

    feat: LoadingStatusWidgetのデザイン更新と動作安定化

--- src/components/widgets/LoadingStatusWidget/index.tsx
+++ src/components/widgets/LoadingStatusWidget/index.tsx
@@ -10,13 +10,24 @@ const FULL_TEXT = "All content loaded successfully"
  * タイピングアニメーションを行うコンポーネント
  * 表示タイミングを少し遅らせたいので子コンポに切り出してマウントタイミングをコントロールしている
  */
-const TypingText = () => {
+const TypingText = ({
+  onTypingAnimationComplete
+}: {
+  /** タイピングアニメーション完了時の処理 */
+  onTypingAnimationComplete: () => void
+}) => {
   const [typedText] = useTypewriter({
     typeSpeed: 40,
     words: [FULL_TEXT]
   })
 
   const hasCompletedTyping = typedText === FULL_TEXT
+
+  useEffect(() => {
+    if (hasCompletedTyping) {
+      onTypingAnimationComplete?.()
+    }
+  }, [hasCompletedTyping, onTypingAnimationComplete])
 
   if (hasCompletedTyping) {
     return <>{FULL_TEXT}</>
@@ -32,19 +43,25 @@ const TypingText = () => {
 /** ローディングステータスウィジェット */
 export const LoadingStatusWidget = () => {
   const [isReadyToStart, setIsReadyToStart] = useState(false)
+  const [isTypingCompleted, setIsTypingCompleted] = useState(false)
 
   useEffect(() => {
     const timerId = window.setTimeout(() => setIsReadyToStart(true), 500)
-
     return () => window.clearTimeout(timerId)
   }, [])
 
   return (
     <div className={styles.loadingStatusWidget}>
-      <span className={styles.text}>{isReadyToStart ? <TypingText /> : null}</span>
-      <span className={styles.icon}>
-        <CheckMarkIcon />
+      <span className={styles.text}>
+        {isReadyToStart ? (
+          <TypingText onTypingAnimationComplete={() => setIsTypingCompleted(true)} />
+        ) : null}
       </span>
+      {isTypingCompleted && (
+        <span className={styles.icon}>
+          <CheckMarkIcon />
+        </span>
+      )}
     </div>
   )
 }
